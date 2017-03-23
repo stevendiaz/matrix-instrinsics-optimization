dfd0559ca24ca575c014416053ae9ca793dee58a
Estevan Diaz <sdiaz@cs.utexas.edu>
2017-03-22 22:20:32 -0500

    register tiling implemented

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <x86intrin.h>
+#include <string.h>
 
 
 void a(int N)
@@ -9,6 +10,8 @@ void a(int N)
     float (*B)[N] = malloc(sizeof(float[N][N]));
     float (*C)[N] = malloc(sizeof(float[N][N]));
 
+    int NU, MU = 64/sizeof(float);
+    printf("nu: %d\n", NU);
     int i,j,k;
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
@@ -26,12 +29,49 @@ void a(int N)
 
 int main(int args, char *argv[]) 
 {
-    float (*A)[4] = malloc(sizeof(double[4][4]));
-    A[0][0] = 1.0;
-    A[2][2] = 2.0;
-    __m64 x;
-    printf("first: %f \n", A[0][0]);
-    printf("second: %f \n", A[2][2]);
+    /* Data & parameter initialization */
+    int i, j, k, m, n;
+    int NU = 4;
+    int MU = 4;
+    int N = 8;
+    float (*A)[N] = malloc(sizeof(float[N][N]));
+    float (*B)[N] = malloc(sizeof(float[N][N]));
+    float (*C)[N] = malloc(sizeof(float[N][N]));
+    for(i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            A[i][j] = 1;
+            B[i][j] = 2;
+        }
+    }
+
+    /* Register tiling, no vectorization */
+    for (i = 0; i < N; i += MU ) {
+        for (j = 0; j < N; j += NU) {
+            for (k = 0; k < N; k++) {
+                // Register tiling
+                // No vectorization
+                for (m = i; m < i + MU; m++) {
+                   for(n = j; n < j + NU; n++) {
+                       __m128 rX = _mm_load_ss(&A[m][k]);
+                       __m128 rY = _mm_load_ss(&B[k][n]);
+                       __m128 rZ = _mm_mul_ss(rX, rY);
+                       rZ = _mm_add_ss(rZ, _mm_load_ss(&C[m][n]));
+                       _mm_store_ss(&C[m][n], rZ);
+                    }
+                }
+            }
+        }
+    }
+    /* Checking output */
+    for(i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%f ", C[i][j]);
+        }
+        printf("\n");
+    }
+
     free(A);
+    free(B);
+    free(C);
     return 0;
 }
