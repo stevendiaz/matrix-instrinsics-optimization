7a0bdbb6b751d9bd77a1c34839ea945a6e4a5211
Eryn Li <erynli@utexas.edu>
2017-03-24 19:55:54 -0500

    implemented PAPI counters for (b)

--- src/main.c
+++ src/main.c
@@ -2,57 +2,86 @@
 #include <stdio.h>
 #include <x86intrin.h>
 #include <string.h>
+#include <papi.h>
+
+void printCounters(long long* counters){
+  printf("Number of flops: %lld\n", counters[0]);
+  printf("Number of L1 data/ins cache misses: %lld\n", counters[1]);
+}
 
 
-void register_tiling(int N)
+void register_tiling(int N, int* PAPI_events)
 {
-    int i, j, k, m, n;
-    int NU = 4;
-    int MU = 4;
-    float (*A)[N] = malloc(sizeof(float[N][N]));
-    float (*B)[N] = malloc(sizeof(float[N][N]));
-    float (*C)[N] = malloc(sizeof(float[N][N]));
 
-    /* Register tiling, no vectorization */
-    for (i = 0; i < N; i += MU ) {
-        for (j = 0; j < N; j += NU) {
-            for (k = 0; k < N; k++) {
-                // Register tiling
-                // No vectorization
-                for (m = i; m < i + MU; m++) {
-                   for(n = j; n < j + NU; n++) {
-                       __m128 rX = _mm_load_ss(&A[m][k]);
-                       __m128 rY = _mm_load_ss(&B[k][n]);
-                       __m128 rZ = _mm_mul_ss(rX, rY);
-                       rZ = _mm_add_ss(rZ, _mm_load_ss(&C[m][n]));
-                       _mm_store_ss(&C[m][n], rZ);
-                    }
-                }
-            }
-        }
-    }
-    /* Checking output */
-    for(i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            printf("%f ", C[i][j]);
-        }
-        printf("\n");
-    }
+  /* Initialize PAPI counter */
+  long long counters[2];
+  memset(counters, 0, 2*sizeof(long long));
+  
 
-    free(A);
-    free(B);
-    free(C);
- 
+  int i, j, k, m, n;
+  int NU = 4;
+  int MU = 4;
+  float (*A)[N] = malloc(sizeof(float[N][N]));
+  float (*B)[N] = malloc(sizeof(float[N][N]));
+  float (*C)[N] = malloc(sizeof(float[N][N]));
+  
+  //Start counters
+  PAPI_start_counters(PAPI_events, 2);  
+
+  /* Register tiling, no vectorization */
+  for (i = 0; i < N; i += MU ) {
+    for (j = 0; j < N; j += NU) {
+      for (k = 0; k < N; k++) {
+	// Register tiling
+	// No vectorization
+	for (m = i; m < i + MU; m++) {
+	  for(n = j; n < j + NU; n++) {
+	    __m128 rX = _mm_load_ss(&A[m][k]);
+	    __m128 rY = _mm_load_ss(&B[k][n]);
+	    __m128 rZ = _mm_mul_ss(rX, rY);
+	    rZ = _mm_add_ss(rZ, _mm_load_ss(&C[m][n]));
+	    _mm_store_ss(&C[m][n], rZ);
+	  }
+	}
+      }
+    }
+  }
+
+  //Stop counters
+  PAPI_stop_counters(counters, 2);
+  printCounters(counters);
+
+
+  /* Checking output */
+  for(i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      printf("%f ", C[i][j]);
+    }
+    printf("\n");
+  }
+  
+  free(A);
+  free(B);
+  free(C);
+  
 }
 
 
 int main(int args, char *argv[]) 
 {
-    /* Data & parameter initialization */
-    int n;
-    for(n = 4; n < 5; n += 4) {
-        register_tiling(n);
-    }
+  /* Initalize PAPI counters */
+  int PAPI_events[] = {
+    PAPI_FP_OPS,
+    PAPI_L1_DCM,
+  };
 
-    return 0;
+  PAPI_library_init(PAPI_VER_CURRENT);
+
+  /* Data & parameter initialization */
+  int n;
+  for(n = 4; n < 5; n += 4) {
+    register_tiling(n, PAPI_events);
+  }
+  
+  return 0;
 }
