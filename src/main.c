5e0d075e3754d64e3b06ef639ef8d0277ef03a5e
Eryn Li <erynli@utexas.edu>
2017-03-24 20:29:52 -0500

    implemented part a

--- src/main.c
+++ src/main.c
@@ -4,12 +4,60 @@
 #include <string.h>
 #include <papi.h>
 
-void printCounters(long long* counters){
+void printCounters(int q, long long* counters){
+  printf("Part %c:", 'a' + q);
   printf("Number of flops: %lld\n", counters[0]);
   printf("Number of L1 data/ins cache misses: %lld\n", counters[1]);
 }
 
+/* Part a  */
 
+void mmm(int N, int* PAPI_events){
+  /* Initialize PAPI counter */
+  long long counters[2];
+  memset(counters, 0, 2*sizeof(long long));
+  
+
+  int i, j, k;
+  float (*A)[N] = malloc(sizeof(float[N][N]));
+  float (*B)[N] = malloc(sizeof(float[N][N]));
+  float (*C)[N] = malloc(sizeof(float[N][N]));
+  memset(A, 0, N*N*sizeof(float));
+  memset(B, 0, N*N*sizeof(float));
+  memset(C, 0, N*N*sizeof(float));
+  
+  //Start counters
+  PAPI_start_counters(PAPI_events, 2);  
+
+  /* Register tiling, no vectorization */
+  for (i = 0; i < N; i++ ) {
+    for (j = 0; j < N; j++) {
+      for (k = 0; k < N; k++) {
+	C[i][j] = C[i][j] + A[i][k] * B[k][j];
+      }
+    }
+  }
+
+  //Stop counters
+  PAPI_stop_counters(counters, 2);
+  printCounters(0, counters);
+
+  /* Checking output */
+  for(i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      printf("%f ", C[i][j]);
+    }
+    printf("\n");
+  }
+  
+  free(A);
+  free(B);
+  free(C);
+}
+
+
+
+/* Part b */
 void register_tiling(int N, int* PAPI_events)
 {
 
@@ -24,11 +72,16 @@ void register_tiling(int N, int* PAPI_events)
   float (*A)[N] = malloc(sizeof(float[N][N]));
   float (*B)[N] = malloc(sizeof(float[N][N]));
   float (*C)[N] = malloc(sizeof(float[N][N]));
+  memset(A, 0, N*N*sizeof(float));
+  memset(B, 0, N*N*sizeof(float));
+  memset(C, 0, N*N*sizeof(float));
+
   
   //Start counters
   PAPI_start_counters(PAPI_events, 2);  
 
   /* Register tiling, no vectorization */
+  int t = 0;
   for (i = 0; i < N; i += MU ) {
     for (j = 0; j < N; j += NU) {
       for (k = 0; k < N; k++) {
@@ -36,6 +89,7 @@ void register_tiling(int N, int* PAPI_events)
 	// No vectorization
 	for (m = i; m < i + MU; m++) {
 	  for(n = j; n < j + NU; n++) {
+	    ++t;
 	    __m128 rX = _mm_load_ss(&A[m][k]);
 	    __m128 rY = _mm_load_ss(&B[k][n]);
 	    __m128 rZ = _mm_mul_ss(rX, rY);
@@ -47,9 +101,11 @@ void register_tiling(int N, int* PAPI_events)
     }
   }
 
+  printf("t = %d", t);
+
   //Stop counters
   PAPI_stop_counters(counters, 2);
-  printCounters(counters);
+  printCounters(1, counters);
 
 
   /* Checking output */
@@ -80,6 +136,7 @@ int main(int args, char *argv[])
   /* Data & parameter initialization */
   int n;
   for(n = 4; n < 5; n += 4) {
+    //mmm(n, PAPI_events);
     register_tiling(n, PAPI_events);
   }
   
